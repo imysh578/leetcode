ddf86e60ecb5bf82f69f968f71c71096b06c508e
Seokhun Yoon <imysh578@gmail.com>
2024-05-06 23:20:09 +0900

    Time: 1 ms (84.09%), Space: 2.2 MB (46.21%) - LeetHub

---
+++ Easy/1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.rs
@@ -0,0 +1,11 @@
+impl Solution {
+    pub fn running_sum(nums: Vec<i32>) -> Vec<i32> {
+        let mut result: Vec<i32> = Vec::new();
+        let mut sum = 0;
+        for num in nums {
+            sum += num;
+            result.push(sum);
+        }
+        result
+    }
+}
