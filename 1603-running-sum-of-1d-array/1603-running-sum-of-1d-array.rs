42c90d80f3ff60e81df75816d1cc341d94e58129
Seokhun Yoon <imysh578@gmail.com>
2024-05-06 23:05:35 +0900

    Time: 1 ms (84.09%), Space: 2.1 MB (46.21%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.rs
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.rs
@@ -1,19 +1,10 @@
 impl Solution {
     pub fn running_sum(nums: Vec<i32>) -> Vec<i32> {
-        let mut result: Vec<i32> = vec![];
+        let mut result: Vec<i32> = Vec::new();
+        let mut sum = 0;
 
-        let iteration = nums.len();
-        let mut count = 0;
-        
-        while count < iteration {
-            let mut sum = 0;
-            for (position, num) in nums.iter().enumerate() {
-                sum += *num;
-                if count == position {
-                    break;
-                }
-            }
-            count+=1;
+        for num in nums {
+            sum += num;
             result.push(sum);
         }
         result
