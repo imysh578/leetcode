faec5f65c40829784127d93afae71120b408f43f
Seokhun Yoon <imysh578@gmail.com>
2024-06-24 00:21:03 +0900

    Time: 1 ms (79.86%), Space: 2.2 MB (54.43%) - LeetHub

--- Easy/0014-longest-common-prefix/0014-longest-common-prefix.rs
+++ Easy/0014-longest-common-prefix/0014-longest-common-prefix.rs
@@ -4,13 +4,19 @@ impl Solution {
             return "".to_string();
         }
 
+        // 첫 번째 문자열을 기준으로 초기 접두사를 설정합니다.
         let first_str = &strs[0];
-        let prefix_max_len = first_str.len();
+        let mut prefix_max_len = first_str.len();
+
+        // 첫 번째 문자열의 문자들을 슬라이스로 접근하여 처리합니다.
+        for word in strs.iter() {
+            prefix_max_len = std::cmp::min(prefix_max_len, word.len());
+        }
 
         for i in 0..prefix_max_len {
             let current_char = first_str.as_bytes()[i];
             for j in 1..strs.len() {
-                if strs[j].len() <= i || strs[j].as_bytes()[i] != current_char {
+                if strs[j].as_bytes()[i] != current_char {
                     return first_str[..i].to_string();
                 }
             }
