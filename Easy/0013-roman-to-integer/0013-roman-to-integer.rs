56ed782491f1394fb1ba350bf0607fe04717aa38
Seokhun Yoon <imysh578@gmail.com>
2024-06-23 17:38:07 +0900

    Time: 0 ms (100%), Space: 2 MB (79.32%) - LeetHub

---
+++ Easy/0013-roman-to-integer/0013-roman-to-integer.rs
@@ -0,0 +1,23 @@
+impl Solution {
+    pub fn roman_to_int(s: String) -> i32 {
+        let mut result = 0;
+        let mut prev = 0;
+
+        for c in s.chars().rev() {
+            let current = match c {
+                'I' => 1,
+                'V' => 5,
+                'X' => 10,
+                'L' => 50,
+                'C' => 100,
+                'D' => 500,
+                'M' => 1000,
+                _ => 0,
+            };
+            result += if current < prev {-current} else {current};
+            prev = current;
+        }
+
+        result
+    }
+}
