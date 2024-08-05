03d85f6f5dc637b4ca3f8928bc9d5d7b69cc7e1c
Seokhun Yoon <imysh578@gmail.com>
2024-08-05 23:19:26 +0900

    Time: 5 ms (21.9%), Space: 2.1 MB (26.59%) - LeetHub

--- Easy/0013-roman-to-integer/0013-roman-to-integer.rs
+++ Easy/0013-roman-to-integer/0013-roman-to-integer.rs
@@ -3,8 +3,8 @@ impl Solution {
         let mut result = 0;
         let mut prev = 0;
 
-        for c in s.chars() {
-            let converted_num = match c {
+        for c in s.chars().rev() {
+            let current = match c {
                 'I' => 1,
                 'V' => 5,
                 'X' => 10,
@@ -14,15 +14,8 @@ impl Solution {
                 'M' => 1000,
                 _ => 0,
             };
-
-            println!("{}", converted_num);
-
-            result += if prev < converted_num {
-                converted_num - 2 * prev
-            } else {
-                converted_num
-            };
-            prev = converted_num;
+            result += if current < prev {-current} else {current};
+            prev = current;
         }
 
         result
