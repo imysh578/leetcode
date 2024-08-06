0d5b6b698772838955087aa01d6942b0ce52dc84
Seokhun Yoon <imysh578@gmail.com>
2024-08-06 19:52:25 +0900

    Time: 1 ms (88.01%), Space: 2.5 MB (21.72%) - LeetHub

--- Easy/0001-two-sum/0001-two-sum.rs
+++ Easy/0001-two-sum/0001-two-sum.rs
@@ -1,12 +1,32 @@
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut map = std::collections::HashMap::new();
+        // O(n^2)
+        // for (i, &num1) in nums.iter().enumerate() {
+        //     let complement = target - num1;
+
+        //     for (j, &num2) in nums.iter().enumerate() {
+        //         if i != j && num2 == complement {
+        //             return vec![i as i32, j as i32];
+        //         }
+        //     }
+        // }
+
+        // return vec![];
+        
+
+        // O(n)
+        use std::collections::HashMap;
+        
+        let mut map = HashMap::new();
+
         for (i, &num) in nums.iter().enumerate() {
-            let complement = target - num;
-            match map.get(&complement) {
-                Some(&j) => return vec![j as i32, i as i32],
-                None => map.insert(num, i)
-            };
+            let compliment = target - num;
+
+            if let Some(&j) = map.get(&compliment) {
+                return vec![j as i32, i as i32];
+            }
+
+            map.insert(num, i);
         }
 
         return vec![];
