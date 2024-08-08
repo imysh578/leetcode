10722d830db83a41da28da7ba3fc3fd3bcd838d1
Seokhun Yoon <imysh578@gmail.com>
2024-08-08 22:23:33 +0900

    Time: 1 ms (77.7%), Space: 2.1 MB (76.02%) - LeetHub

---
+++ Easy/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.rs
@@ -0,0 +1,34 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn merge_two_lists(list1: Option<Box<ListNode>>, list2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        match (list1, list2) {
+            (None, None) => None,
+            (None, Some(list2)) => Some(list2),
+            (Some(list1), None) => Some(list1),
+            (Some(mut l1), Some(mut l2)) => {
+                if l1.val < l2.val {
+                    l1.next = Self::merge_two_lists(l1.next, Some(l2));
+                    Some(l1)
+                } else {
+                    l2.next = Self::merge_two_lists(Some(l1), l2.next);
+                    Some(l2)
+                }
+            }
+        }
+    }
+}
