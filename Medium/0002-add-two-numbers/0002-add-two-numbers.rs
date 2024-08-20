77140c00deac35199fe89f94e713881bcfe91a88
Seokhun Yoon <imysh578@gmail.com>
2024-08-20 23:03:26 +0900

    Time: 3 ms (65.34%), Space: 2.1 MB (98.5%) - LeetHub

---
+++ Medium/0002-add-two-numbers/0002-add-two-numbers.rs
@@ -0,0 +1,59 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn add_two_numbers(
+        l1: Option<Box<ListNode>>,
+        l2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        let mut result = ListNode::new(0);
+        let mut pointer = &mut result;
+
+        let list1 = l1.clone();
+        let list2 = l2.clone();
+
+        match (list1, list2) {
+            (None, None) => None,
+            (list1, None) => list1,
+            (None, list2) => list2,
+            (mut list1, mut list2) => {
+                let mut carry = 0;
+                while list1.is_some() || list2.is_some() {
+                    let sum = carry
+                        + list1.as_ref().map_or(0, |node| node.val)
+                        + list2.as_ref().map_or(0, |node| node.val);
+
+                    carry = sum / 10;
+
+                    let new_node = Box::new(ListNode::new(sum % 10));
+
+                    pointer.next = Some(new_node);
+                    pointer = pointer.next.as_mut().unwrap();
+
+                    list1 = list1.and_then(|node| node.next);
+                    list2 = list2.and_then(|node| node.next);
+                }
+
+                if carry > 0 {
+                    let new_node = Box::new(ListNode::new(carry));
+                    pointer.next = Some(new_node);
+                }
+
+                result.next
+            }
+        }
+    }
+}
