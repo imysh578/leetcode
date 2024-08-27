f69e9bdae4b47e244063798aace3096e7b868364
Seokhun Yoon <imysh578@gmail.com>
2024-08-28 00:04:35 +0900

    Time: 0 ms (100%), Space: 2.2 MB (28.72%) - LeetHub

--- Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.rs
+++ Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.rs
@@ -4,19 +4,15 @@ impl Solution {
         use std::collections::HashMap;
 
         let mut map = HashMap::new();
-        let mut prev = 0;
+        let mut prev = -1;
         let mut longest_length = 0;
 
         for (curr, char) in s.chars().enumerate() {
-            let found_index = map.get(&char);
-
-            while found_index.is_some() && *found_index.unwrap() >= prev {
-                prev += 1;
+            if let Some(found_index) = map.insert(char, curr as i32) {
+                prev = max(prev, found_index);
             }
 
-            longest_length = max(longest_length, curr as i32 - prev + 1);
-
-            map.insert(char, curr as i32);
+            longest_length = max(longest_length, curr as i32 - prev);
         }
 
         longest_length
